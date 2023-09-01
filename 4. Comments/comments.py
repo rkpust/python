a66523e9f5196ba6a69f90abb8c1fce6882ef0b2
rkpust <rezaul.cse.pust17@gmail.com>
2023-09-02 00:38:34 +0600

    added rules of comments

---
+++ 4. Comments/comments.py
@@ -0,0 +1,17 @@
+#This is a comment.
+print("Hello, World! 1") #This is a comment
+
+#print("Hello, World!")
+print("Cheers, Mate!") 
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World! 2")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World! 3")
