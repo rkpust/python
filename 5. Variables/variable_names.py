679fca79aa19a1ad3c2fcc73e22c9777a498d3b8
rkpust <rezaul.cse.pust17@gmail.com>
2023-10-12 14:56:50 +0600

    Variable Names Rules

---
+++ 5. Variables/variable_names.py
@@ -0,0 +1,35 @@
+#Legal variable names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+
+# Illegal variable names
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+
+#This example will produce an error in the result
+
+
+# Multi Words Variable Names
+myVariableName = "John"
+MyVariableName = "John"
+my_variable_name = "John"
+
+
+print(myVariableName)
+print(MyVariableName)
+print(my_variable_name)
+
